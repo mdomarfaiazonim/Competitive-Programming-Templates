8a8f39b29fbcd4727bdd8cd4d3c7d95c8de9af5e
MOHAMMED OMAR FAIAZ <fightforUnderdogs@gmail.com>
2025-05-15 15:14:22 +0600

    added lca function

--- lca.cpp
+++ lca.cpp
@@ -121,7 +121,15 @@ bool is_ancestor(int u, int v)
 {
     return tin[u] <= tin[v] && tout[u] >= tout[v];
 }
-
+int lca(int u, int v) {
+  if (is_ancestor(u, v)) return u;
+  if (is_ancestor(v, u)) return v;
+  for (int i = l; i >= 0; --i) {
+    if (!is_ancestor(up[u][i], v))
+      u = up[u][i];
+  }
+  return up[u][0];
+}
 void preprocess(int root) {
     timer = 0;
     l = ceil(log2(n));
